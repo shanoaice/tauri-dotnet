51cdb0ee5479e3cf46f65799929eea4304051623
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-12-10 12:24:03 +0800

    chore(init): init project

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![feature(vec_into_raw_parts)]
+mod api;
+mod ffi;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
