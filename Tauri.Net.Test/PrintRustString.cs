260cbc3ec9cb9b47ea7e26e984c1e5c5cd990778
shanoaice <25174488+shanoaice@users.noreply.github.com>
2023-12-11 23:22:18 +0800

    test(net-string): .NET string interop test

---
+++ Tauri.Net.Test/PrintRustString.cs
@@ -0,0 +1,22 @@
+﻿namespace Tauri.Net.Test;
+
+using Tauri.Net;
+
+public class PrintRustString
+{
+    public delegate void PrintStringDelegate(UnmanagedStringStruct rustStringStruct);
+
+    public delegate Utf16InteropStringStruct ReturnStringDelegate();
+    
+    public static void PrintString (UnmanagedStringStruct rustStringStruct)
+    {
+        using var rustString = new UnmanagedString(rustStringStruct);
+        Console.WriteLine(rustString.ToString());
+    }
+
+    public static Utf16InteropStringStruct ReturnString()
+    {
+        const string str = "Hello Rust!";
+        return Utf16InteropString.FromString(str);
+    }
+}
